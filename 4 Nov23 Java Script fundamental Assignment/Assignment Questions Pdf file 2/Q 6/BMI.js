e208febb21c45f70b54211a8e97a7459dcd565bb
Omi160 <152854745+Omi160@users.noreply.github.com>
2024-02-11 01:05:50 +0530

    Solved assignment files are added
    4 Nov"23 Java Script fundamental Assignment

---
+++ 4 Nov23 Java Script fundamental Assignment/Assignment Questions Pdf file 2/Q 6/BMI.js
@@ -0,0 +1,4 @@
+const height = 172
+const weight = 57
+const BMI = weight/height*height
+console.log("BMI =",BMI);
---
+++ 4 Nov23 Java Script fundamental Assignment/Assignment Questions Pdf file 2/Q 7/circle.js
@@ -0,0 +1,4 @@
+const radius = 10;
+const pi = Math.pi;
+const area = pi * Math.pow(radius, 2); 
+console.log("Area of the circle:", area);
